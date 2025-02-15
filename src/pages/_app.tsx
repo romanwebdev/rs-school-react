49d604d52a19b7e9374a7e32564498f64cc6297e
romanwebdev101 <romanwebdev101@gmail.com>
2025-02-15 13:42:48 +0200

    feat: migrate header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import { AppProps } from 'next/app';
+import { ThemeProvider } from '../context';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider>
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
+import Header from '../components/Header';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.home} data-testid="home">
       <div className={styles.mainWrap}>
-        <h1>Home Page</h1>
-        {/* <Header />
-        <Main /> */}
+        <Header />
+        {/* <Main /> */}
       </div>
       {/* <Outlet /> */}
     </div>
