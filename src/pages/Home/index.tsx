c2eca837b59ea953cfc46ebf6c263af6942f6835
romanwebdev101 <romanwebdev101@gmail.com>
2025-02-24 18:31:03 +0200

    feat: migrate to react router

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,23 @@
+import { Provider } from 'react-redux';
 import { Outlet } from 'react-router';
 import Header from '../../components/Header';
 import Main from '../../components/Main';
+import { ThemeProvider } from '../../context';
+import { store } from '../../store';
 import styles from './Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.home} data-testid="home">
-      <div className={styles.mainWrap}>
-        <Header />
-        <Main />
-      </div>
-      <Outlet />
-    </div>
+    <ThemeProvider>
+      <Provider store={store}>
+        <div className={styles.home} data-testid="home">
+          <div className={styles.mainWrap}>
+            <Header />
+            <Main />
+          </div>
+          <Outlet />
+        </div>
+      </Provider>
+    </ThemeProvider>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 /// <reference types="vitest" />
-import react from '@vitejs/plugin-react';
+import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactRouter()],
   test: {
     globals: true,
     environment: 'jsdom',
